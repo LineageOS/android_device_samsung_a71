c94c91d85ba73e4264d48f579622aa35886ebfac
Haky86 <hakamelassouad@gmail.com>
2024-10-02 09:31:10 +0200

    a71: Rename the service binary of NXP's rc
    * As part of https://review.lineageos.org/c/LineageOS/android_device_samsung_a71/+/404593.
    
    Change-Id: I7c8b21c39d90d78286b24d07d37cf596d9485f7b

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/etc/init/nxp.android.hardware.nfc@1.1-service.samsung.rc)
+            [ "$2" = "" ] && return 0
+            sed -i "s/nxp.android.hardware.nfc@1.1-service/nxp.android.hardware.nfc@1.1-service.samsung/" "${2}"
+            ;;
+    esac
+
+    return 0
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
