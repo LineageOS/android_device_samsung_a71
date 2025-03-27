e6efd16182de849b6f7e75d3c52bccd91bd6e86a
Haky86 <hakamelassouad@gmail.com>
2025-03-27 21:54:53 +0100

    a71: Create camera firmware symlink
    * It does not affect on camera as it works without issues.
    * Fixes build errors related to copying .bin files on 15 QPR2:
    error: build/soong/fsgen/Android.bp:31:1: module "lineage_a71_generated_vendor_image" variant "android_common" (created by module "soong_filesystem_creator" variant "andro
    id_common"): packaging conflict at lib64/camera/com.samsung.tuned.lsi_s5kgw1s.bin
    
    Change-Id: I79ea0c6214bb462d0949b44b15020e5db3cbec5d

--- device.mk
+++ device.mk
@@ -13,6 +13,23 @@ $(call inherit-product, device/samsung/a71-common/common.mk)
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/rootdir/vendor/etc/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml \
 
+# Camera firmware symlinks
+PRODUCT_PACKAGES += \
+    com.samsung.sensormodule.0_0_lsi_s5kgw1s_new.bin_symlink \
+    com.samsung.sensormodule.0_0_lsi_s5kgw1s_old.bin_symlink \
+    com.samsung.sensormodule.0_1_sony_imx682.bin_symlink \
+    com.samsung.sensormodule.1_sony_imx616.bin_symlink \
+    com.samsung.sensormodule.2_hynix_hi1336.bin_symlink \
+    com.samsung.sensormodule.3_lsi_gc5035.bin_symlink \
+    com.samsung.sensormodule.4_lsi_gc5035_macro.bin_symlink \
+    com.samsung.sensormodule.8_sony_imx616_full.bin_symlink \
+    com.samsung.tuned.hynix_hi1336.bin_symlink \
+    com.samsung.tuned.lsi_gc5035.bin_symlink \
+    com.samsung.tuned.lsi_gc5035_macro.bin_symlink \
+    com.samsung.tuned.lsi_s5kgw1s.bin_symlink \
+    com.samsung.tuned.sony_imx616.bin_symlink \
+    com.samsung.tuned.sony_imx682.bin_symlink \
+
 # Fingerprint
 TARGET_HAS_UDFPS := true
 
